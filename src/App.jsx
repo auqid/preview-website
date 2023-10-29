d6374122fc720f42dca5e9f31ec97ea2120be352
auqid <auqidirfan100@gmail.com>
2023-10-29 20:54:09 +0530

    added props and imporved file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
+{/**Import Images like this to avoid Deployment issues*/}
+import reactImg from './assets/react-core-concepts.png'
+import Header from './components/Header';
+import { CORE_CONCEPTS } from './data.js'
+import CoreConcept from './components/CoreConcepts';
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header/>
+      
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            {/**Getting an object from Data.js and passing its keyValue pairs as props. */}
+            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+            {/**Different way using a spread operator */}
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+            
+          </ul>
+        </section>
       </main>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import reactImg from '../assets/react-core-concepts.png'
+import './header.css'
+
+
+
+const reactDescription = ['Fundamental','Crucial','Core']
+
+{/**using a function to get a random number and using that in jsx to get a random value in a array */}
+function getRandomInt(max){
+    return Math.floor(Math.random()*(max+1))
+  }
+
+function Header (){
+    const description = reactDescription[getRandomInt(2)];
+    return (<header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>)
+   }
+   {/**object destructuring*/}
+   
+export default Header
